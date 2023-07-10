8c597ffbc0e9143810779602d925c632c57fa7c8
Muhammad Gibran <gibramuh@gmail.com>
2023-07-10 17:33:33 +0700

    remove dd in controller

--- src/Http/Controllers/Dashboard/DashboardController.php
+++ src/Http/Controllers/Dashboard/DashboardController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function hrdlive(){
-        dd("yaya");
+        // dd("yaya");
 
         $title = "DigiForSDI | Dashboard";
 
